4647fd44f88e1594f2f4aea88d028d432c83d13c
andrew leong <andrew.leong@jewelpaymentech.com>
2019-06-01 22:43:08 +0800

    Add env folder

---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/andrewleong/Documents/personal-projects/django-helpdesk-api/env/bin/python3.6
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
